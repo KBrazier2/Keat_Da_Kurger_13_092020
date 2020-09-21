56a3a749c7de9cf97505a580dd201fba8a54c474
ksbrazier <kylebrazier@gmail.com>
2020-09-20 20:23:05 -0400

    seed issue

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger) VALUES ('The Silentil Night Burger (comes with lentils)', FALSE);
-INSERT INTO burgers (burger) VALUES ('Baby You Can Chive My Car Burger', FALSE);
-INSERT INTO burgers (burger) VALUES ('Pepper Dont Preach Burger(with pepper)', FALSE);
+INSERT INTO burgers (burger_name, inhaled) VALUES ('The Silentil Night Burger (comes with lentils)', FALSE);
+INSERT INTO burgers (burger_name, inhaled) VALUES ('Baby You Can Chive My Car Burger', FALSE);
+INSERT INTO burgers (burger_name, inhaled) VALUES ('Pepper Dont Preach Burger(with pepper)', FALSE);
