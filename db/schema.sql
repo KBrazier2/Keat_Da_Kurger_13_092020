90f387de1b9a7f5c687f1b435545b8048d50ee48
ksbrazier <kylebrazier@gmail.com>
2020-09-20 20:26:43 -0400

    burger_name

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS burgers;
 
 CREATE TABLE burgers (
     id int NOT NULL AUTO_INCREMENT,
-    burger varchar(255) NOT NULL,
+    burger_name varchar(255) NOT NULL,
     inhaled BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
