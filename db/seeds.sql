3f3c8e5c194caf014add0078b56d58b95d9d38a8
ksbrazier <kylebrazier@gmail.com>
2020-09-24 11:04:11 -0400

    changed seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, inhaled) VALUES ('The Silentil Night Burger (comes with lentils)', FALSE);
+INSERT INTO burgers (burger_name, inhaled) VALUES ('If Looks Could Kale Burger', FALSE);
 INSERT INTO burgers (burger_name, inhaled) VALUES ('Baby You Can Chive My Car Burger', FALSE);
-INSERT INTO burgers (burger_name, inhaled) VALUES ('Pepper Dont Preach Burger(with pepper)', FALSE);
+INSERT INTO burgers (burger_name, inhaled) VALUES ('Gourdon Hamsey', FALSE);
