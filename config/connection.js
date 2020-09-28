aaf26ce3e99726e6323a9189c01afcab23e8d510
ksbrazier <kylebrazier@gmail.com>
2020-09-28 11:23:19 -0400

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,5 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Password",
-    database: "burgers_db"
-});
+var connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
